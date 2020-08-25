214b692b59f6491252294f436bbec44ffeaf3473
wangqin <347656409nick@gmail.com>
2020-08-26 00:08:09 +0800

    Add new entry to the main function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,12 @@ import ui.TextUi;
 
 public class Main {
     public static void main(String[] args) {
-        TextUi.showGreetings();
+        TextUi.showLogo();
+        new Main().run();     //quite dummy here, will change later
         TextUi.showFarewells();
+    }
+
+    private void run() {
+        TextUi.showGreetings();
     }
 }
--- src/main/java/ui/TextUi.java
+++ src/main/java/ui/TextUi.java
@@ -4,6 +4,11 @@ import util.Message;
 
 public class TextUi {
 
+    public static void showLogo() {
+        printDivider();
+        System.out.print(Message.LOGO);
+    }
+
     public static void showGreetings() {
         printDivider();
         System.out.print(Message.MESSAGE_WELCOME);
