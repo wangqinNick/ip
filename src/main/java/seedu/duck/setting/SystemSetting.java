c6371090394b0948d35652c3505216ee42a26c50
wangqinNick <347656409nick@gmail.com>
2020-09-20 03:14:55 +0800

    Add JavaDoc for SystemSetting

--- src/main/java/seedu/duck/setting/SystemSetting.java
+++ src/main/java/seedu/duck/setting/SystemSetting.java
@@ -5,14 +5,27 @@ import seedu.duck.util.Language;
 public class SystemSetting {
     private static Language systemLanguage;
 
+    /**
+     * Initialises the system setting
+     * Currently only the language is implemented
+     * Sets the system language to English
+     */
     public static void initialise(){
         systemLanguage = Language.ENGLISH;
     }
 
+    /**
+     * Returns the current system language
+     * @return the current system language
+     */
     public static Language getSystemLanguage() {
         return systemLanguage;
     }
 
+    /**
+     * Sets the system language to a specific language
+     * @param language a specific language
+     */
     public static void setSystemLanguage(Language language) {
         systemLanguage = language;
     }
